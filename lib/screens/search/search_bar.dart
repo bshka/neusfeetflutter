6b6c01c12e9774687dca6e71f1b52efc2286ffbb
Dmitry Golovko <dzmitry.halauko@itechart-group.com>
2019-10-07 09:52:18 +0300

    added ios search bar

--- lib/screens/search/search_bar.dart
+++ lib/screens/search/search_bar.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nues_feet_flutter/i18n/strings.dart';
 import 'package:nues_feet_flutter/screens/articles/articles_list.dart';
@@ -13,33 +16,73 @@ class _SearchBarState extends State<SearchBar> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 15, vertical: 8),
-      child: Material(
-        elevation: 4,
+      child: Platform.isIOS ? _iOSTextField() : _androidTextField(),
+    );
+  }
+
+  void _onTextChanged(String query) {
+    ArticlesListProvider.of(context).search(query);
+  }
+
+  Widget _androidTextField() {
+    return Material(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Container(
+        height: 40,
+        child: TextField(
+          textInputAction: TextInputAction.search,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(0),
+            prefixIcon: Icon(
+              Icons.search,
+              color: Colors.kMartini,
+            ),
+            hintStyle: TextStyle(
+              color: Colors.kMartini,
+            ),
+            hintText: Strings.of(context).searchHint,
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+          ),
+          onChanged: _onTextChanged,
+        ),
+      ),
+    );
+  }
+
+  Widget _iOSTextField() {
+    return Container(
+      decoration: ShapeDecoration(
+        shadows: [
+          BoxShadow(
+            color: Color(0x3F000000),
+            blurRadius: 5,
+            spreadRadius: 1,
+            offset: Offset(0, 2),
+          )
+        ],
+        color: Colors.kWhite,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(100),
         ),
-        child: Container(
-          height: 40,
-          child: TextField(
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(0),
-              prefixIcon: Icon(
-                Icons.search,
-                color: Colors.kMartini,
-              ),
-              hintStyle: TextStyle(
-                color: Colors.kMartini,
-              ),
-              hintText: Strings.of(context).searchHint,
-              border: OutlineInputBorder(
-                borderSide: BorderSide.none,
-              ),
-            ),
-            onChanged: (query) {
-              ArticlesListProvider.of(context).search(query);
-            },
-          ),
+      ),
+      height: 40,
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: CupertinoTextField(
+        onChanged: _onTextChanged,
+        maxLines: 1,
+        autofocus: false,
+        autocorrect: true,
+        textInputAction: TextInputAction.search,
+        prefix: Icon(
+          Icons.search,
+          color: Colors.kMartini,
         ),
+        clearButtonMode: OverlayVisibilityMode.editing,
       ),
     );
   }
